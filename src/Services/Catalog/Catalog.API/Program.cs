5309ea0222789db3934ab42c9e5e5bb68d58c053
kitouanna@gmail.com <kitouanna@gmail.com>
2025-09-02 23:55:25 +0300

    adding exception handler 1

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,7 @@
 using BuildingBlocks.Behaviors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +28,25 @@ var app = builder.Build();
 // Configure the HTTP request pipeline
 
 app.MapCarter();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+    });
+});
 
 app.Run();
