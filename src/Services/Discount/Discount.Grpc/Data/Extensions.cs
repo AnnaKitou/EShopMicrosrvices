64dfe588511e66bf5634024bacd6ffc6afc089b6
kitouanna@gmail.com <kitouanna@gmail.com>
2025-12-07 17:17:26 +0200

    grpc datacontext - migration / set up

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            dbContext.Database.MigrateAsync();
+            return app;
+        }
+    }
+}
